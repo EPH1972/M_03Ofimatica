acf09f704c4b5a3fbc5d62b7b6d7a0f1d8023c52
EPH1972 <44460768+EPH1972@users.noreply.github.com>
2018-10-29 18:44:46 +0100

    Update PRACTICA 3.py

--- PRACTICA 3.py
+++ PRACTICA 3.py
@@ -1,1 +1,10 @@
-
+edad = int(input("¿Dime tu edad?"))
+sexe = input("¿Eres H o M?")
+color = int(input("¿Cual es el color de tu pelo? (Rubio, Pelirojo, Moreno)"))
+jubilado=input("¿Te has jubilado?")
+if ((sexe=="H")and(jubilado=="no")):
+    print("Preció: 1€")
+if (((sexe=="M")and(color=="Moreno")and(jubilado=="si"))):
+   print("Gratis")
+if((sexe=="M")and(jubilado=="si")and(color=="Pelirojo")):
+   print("Precio: 0.50€")
