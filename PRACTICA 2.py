3c74333840b04f64ba880a6df40de50a27a988a1
EPH1972 <44460768+EPH1972@users.noreply.github.com>
2018-10-29 18:33:48 +0100

    Update PRACTICA 2.py

--- PRACTICA 2.py
+++ PRACTICA 2.py
@@ -1,8 +1,1 @@
-Jamon = input("¿Has comprado jamon?")
-Abduccion = input("¿Te han abducido?")
-Dia = input("¿Hoy es jueves?")
-Estudiar = input("¿Has estudiado mucho?")
-if(Jamon == "si")or((Abduccion == "si")and(Dia == "J"))or(Estudiar == "mucho")
-  print("Has aprobado M_03")
-else:
-  print("No has aprobado M_03")
+
