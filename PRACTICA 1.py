5ac00c58420bd128462f966e502c9fab9b6e0240
EPH1972 <44460768+EPH1972@users.noreply.github.com>
2018-10-29 18:34:02 +0100

    Update PRACTICA 1.py

--- PRACTICA 1.py
+++ PRACTICA 1.py
@@ -3,3 +3,12 @@ if dinero < 11 :
   print ("Mama dame dinero")
 else:
   print ("Tengo dinero suficiente")
+
+  Jamon = input("¿Has comprado jamon?")
+Abduccion = input("¿Te han abducido?")
+Dia = input("¿Hoy es jueves?")
+Estudiar = input("¿Has estudiado mucho?")
+if(Jamon == "si")or((Abduccion == "si")and(Dia == "J"))or(Estudiar == "mucho")
+  print("Has aprobado M_03")
+else:
+  print("No has aprobado M_03")
