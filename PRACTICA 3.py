8ae0919a68071c3384e3a80ece056a6607da7015
EPH1972 <44460768+EPH1972@users.noreply.github.com>
2018-10-29 21:23:54 +0100

    Update PRACTICA 3.py

--- PRACTICA 3.py
+++ PRACTICA 3.py
@@ -1,1 +1,5 @@
-
+edad = int(input("¿Que edad tienes?:"))
+if(edad >= 18) and (edad <= 23) or (edad == 17)):
+  print("Puedes entarar en la sesión de jovenes")
+else:
+  print("Lo siento pero no puedes entrar en la sesión de jovenes")
