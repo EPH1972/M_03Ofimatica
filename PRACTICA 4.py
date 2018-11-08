9bddcb720246e61df16bfc2424e892f22aef7fc3
EPH1972 <44460768+EPH1972@users.noreply.github.com>
2018-11-08 08:38:37 +0100

    Update PRACTICA 4.py

--- PRACTICA 4.py
+++ PRACTICA 4.py
@@ -1,7 +1,5 @@
-Anyo_actual = int(input("En que año estamos?"))
-Anyo_cualquiera = int(input("Dime un año cualquiera?"))
-if(Anyo_actual == 0)or(Anyo_cualquiera == 0):
-    print("El año 0 no existe")
+Anyo_actual = int(input("En que año estamos:"))
+Anyo_cualquiera = int(input("Dime un año cualquiera:"))
 if (Anyo_actual == Anyo_cualquiera):
     print("Son el mismo año!")
 if(Anyo_actual > Anyo_cualquiera):
@@ -9,4 +7,7 @@ if(Anyo_actual > Anyo_cualquiera):
 if(Anyo_actual < Anyo_cualquiera):
     print("Para llegar al año",Anyo_actual,"faltan",Anyo_cualquiera - Anyo_actual,"año/años")
 else:
-   print("Error!")
+   if(Anyo_actual == 0)or(Anyo_cualquiera == 0):
+       print("El año 0 no existe")
+   else:
+       print("Error!")
