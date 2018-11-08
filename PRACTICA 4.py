7d2f820b52a26a6ad54d44a5b9a4bdbd5fb0c48d
EPH1972 <44460768+EPH1972@users.noreply.github.com>
2018-11-08 08:59:38 +0100

    Update PRACTICA 4.py

--- PRACTICA 4.py
+++ PRACTICA 4.py
@@ -1,13 +1,13 @@
 Anyo_actual = int(input("En que año estamos:"))
 Anyo_cualquiera = int(input("Dime un año cualquiera:"))
-if (Anyo_actual == Anyo_cualquiera):
-    print("Son el mismo año!")
-if(Anyo_actual > Anyo_cualquiera):
-    print("Desde el año",Anyo_cualquiera,"han pasado",Anyo_actual - Anyo_cualquiera,"año/años")
-if(Anyo_actual < Anyo_cualquiera):
-    print("Para llegar al año",Anyo_actual,"faltan",Anyo_cualquiera - Anyo_actual,"año/años")
+if(Anyo_actual == 0)or(Anyo_cualquiera == 0):
+    print("El año 0 no existe")
 else:
-   if(Anyo_actual == 0)or(Anyo_cualquiera == 0):
-       print("El año 0 no existe")
-   else:
-       print("Error!")
+       if (Anyo_actual == Anyo_cualquiera):
+           print("Son el mismo año!")
+       if(Anyo_actual > Anyo_cualquiera):
+           print("Desde el año",Anyo_cualquiera,"han pasado",Anyo_actual - Anyo_cualquiera - 1,"año/años")
+       if(Anyo_actual < Anyo_cualquiera):
+           print("Para llegar al año",Anyo_actual,"faltan",Anyo_cualquiera - Anyo_actual - 1,"año/años")
+       else:
+           print("Esa es la difernacia entre el año",Anyo_actual,"y el año",Anyo_cualquiera,)
