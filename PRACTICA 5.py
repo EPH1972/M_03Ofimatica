ffd82180e755d0dc62b1e6804f40b75916feae87
EPH1972 <44460768+EPH1972@users.noreply.github.com>
2018-11-13 13:38:31 +0100

    Update PRACTICA 5.py

--- PRACTICA 5.py
+++ PRACTICA 5.py
@@ -1,10 +1,10 @@
 #coding: utf-8
-opcio = input("Que vols calcular T (triangle) o C (cercle):")
+opcio = input("Que quieres calcular T (triangulo) o C (circulo):")
 if(opcio == "C") or (opcio == "c"):
-    radi = float(input("Quin es el radi del teu cercle:"))
-    print("La teva area es" ,3.14 * radi * 2,)
+    radi = float(input("Cual es el radio de tu circulo:"))
+    print("Tu area es" ,3.14 * radi * 2,)
 else:
     if(opcio == "T") or (opcio == "t"):
-        base = float(input("Digues la base del triangle:"))
-        altura = float(input("Digues l'alsada del triangle:"))
-        print("La teva area es",(base*altura) / 2,)
+        base = float(input("Dime la base del triangulo:"))
+        altura = float(input("Dime la altura del triangulo:"))
+        print("Tu area es",(base*altura) / 2,)
